4efb8d9b944705be853e404c4d97461639558e35
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-03-17 02:41:17 +0200

    Tests&Procfile

--- libraryAPI/apis/tests.py
+++ libraryAPI/apis/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from books.models import Book
 
-# Create your tests here.
+
+class APITests(APITestCase):
+    @classmethod
+    def setUp(cls):
+        cls.book = Book.objects.create(
+            title="Fuck Every Thing",
+            subtitle="I hate everybody",
+            author="Ahmed Esmail",
+            isbn="1234567890123",
+        )
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
